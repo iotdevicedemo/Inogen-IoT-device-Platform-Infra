e8ed1faa6a812812e049f1c549cff01ddb354e87
Rishi <dhayalkarrish@gmail.com>
2026-09-08 23:47:03 -0700

    Wire DEV Resource Group and Event Grid modules

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,2 +1,18 @@
-# DEV environment entry point.
-# Module wiring will be added in the next step.
+module "resource_group" {
+  source = "../../modules/resource-group"
+
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+module "eventgrid" {
+  source = "../../modules/eventgrid"
+
+  name                       = var.eventgrid_topic_name
+  location                   = var.location
+  resource_group_name        = module.resource_group.name
+  input_schema               = "EventGridSchema"
+  public_network_access_enabled = true
+  tags                       = var.tags
+}
