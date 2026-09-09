fe192d8687def5cc3428d068a8a22d566bf8da53
Rishi <dhayalkarrish@gmail.com>
2026-09-08 23:46:57 -0700

    Define DEV Event Grid variables

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -1,1 +1,20 @@
-# DEV environment variables will be added in the next step.
+variable "location" {
+  description = "Azure region for the DEV environment."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "DEV resource group name."
+  type        = string
+}
+
+variable "eventgrid_topic_name" {
+  description = "DEV Event Grid topic name."
+  type        = string
+}
+
+variable "tags" {
+  description = "Common tags for DEV resources."
+  type        = map(string)
+  default     = {}
+}
