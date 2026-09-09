ed146654d00b1b44d76edf2a9ebe16f8e699d9a4
Rishi <dhayalkarrish@gmail.com>
2026-09-08 23:47:21 -0700

    Add DEV Event Grid outputs

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -1,1 +1,16 @@
-# DEV outputs will be added in the next step.
+output "resource_group_name" {
+  value = module.resource_group.name
+}
+
+output "eventgrid_topic_name" {
+  value = module.eventgrid.name
+}
+
+output "eventgrid_topic_id" {
+  value = module.eventgrid.id
+}
+
+output "eventgrid_topic_endpoint" {
+  value     = module.eventgrid.endpoint
+  sensitive = true
+}
