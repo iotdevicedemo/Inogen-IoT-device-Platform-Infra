196f426347366b232b1c626a6482debff33fa010
Rishi <dhayalkarrish@gmail.com>
2026-09-08 23:46:15 -0700

    Configure Terraform AzureRM provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,1 +1,14 @@
-# AzureRM provider configuration will be added in the next step.
+terraform {
+  required_version = ">= 1.9.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
