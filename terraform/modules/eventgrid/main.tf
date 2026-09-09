176a3cff9897a6baeaa753ee353f0b1e42b281af
Rishi <dhayalkarrish@gmail.com>
2026-09-08 23:46:19 -0700

    Implement Event Grid topic module

--- terraform/modules/eventgrid/main.tf
+++ terraform/modules/eventgrid/main.tf
@@ -1,1 +1,10 @@
-# Event Grid module: topic and event subscription.
+resource "azurerm_eventgrid_topic" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  input_schema              = var.input_schema
+  public_network_access_enabled = var.public_network_access_enabled
+
+  tags = var.tags
+}
